cbc4672ee4641600aed91ddffdac61d35d98f996
eoniedev <eoniedev@gmail.com>
2024-10-28 00:21:10 +0900

    init: 프로젝트 생성

---
+++ src/main/java/com/example/gwpractice/GwPracticeApplication.java
@@ -0,0 +1,13 @@
+package com.example.gwpractice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GwPracticeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GwPracticeApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/gwpractice/GwPracticeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.gwpractice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GwPracticeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
